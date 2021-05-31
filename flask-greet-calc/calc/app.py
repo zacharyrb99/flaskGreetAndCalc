30348ccea28292dafcc2a7516634a22635ccd224
zacharyrb99 <zach.boudreaux999@gmail.com>
2021-05-31 11:59:07 -0400

    finished exercise

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -1,1 +1,19 @@
-# Put your app in here.
+from operations import add,sub,mult,div
+from flask import Flask, request
+app = Flask(__name__)
+
+
+OPERATIONS = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div
+}
+
+@app.route("/math/<operation>")
+def math(operation):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    answer = OPERATIONS[operation](a,b)
+
+    return str(answer)
--- flask-greet-calc/greet/app.py
+++ flask-greet-calc/greet/app.py
@@ -1,1 +1,14 @@
-#app.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/welcome")
+def welcome():
+    return "welcome"
+
+@app.route("/welcome/home")
+def welcome_home():
+    return "welcome home"
+
+@app.route("/welcome/back")
+def welcome_back():
+    return "welcome back"
