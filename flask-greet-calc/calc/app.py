747f4a57b5fb9b115bf2bb9ad40ccb6c72e0c564
zacharyrb99 <zach.boudreaux999@gmail.com>
2021-06-01 02:00:23 -0400

    finished

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -2,7 +2,49 @@ from operations import add,sub,mult,div
 from flask import Flask, request
 app = Flask(__name__)
 
+@app.route("/add")
+def do_add():
+    """Add a and b parameters."""
 
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    result = add(a, b)
+
+    return str(result)
+
+@app.route("/sub")
+def do_sub():
+    """Subtract and b parameters."""
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    result = sub(a, b)
+
+    return str(result)
+
+@app.route("/mult")
+def do_mult():
+    """Multiply a and b parameters."""
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    result = mult(a, b)
+
+    return str(result)
+
+@app.route("/div")
+def do_div():
+    """Divide a and b parameters."""
+
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    result = div(a, b)
+
+    return str(result)
+
+########################################
+#shorter version
+########################################
 OPERATIONS = {
     "add": add,
     "sub": sub,
